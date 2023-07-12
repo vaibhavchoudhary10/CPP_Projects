4414f16c0c4b20d78984dc142688231da1c87311
Vaibhav_Choudhary <vaibhav.choudhary@meu.edu.in>
2023-07-12 16:52:33 +0530

    Complete Code

--- Car-game.cpp
+++ Car-game.cpp
@@ -16,10 +16,10 @@ COORD CursorPosition;
 int enemyY[3];
 int enemyX[3];
 int enemyFlag[3];
-char car[4][4] = {' ','+','+',' ',
-                    '+','+','+','+',
-                    ' ','+','+',' ',
-                    '+','+','+','+',};
+char car[4][4] = {' ','±','±',' ', 
+					'±','±','±','±', 
+					' ','±','±',' ',
+					'±','±','±','±'};
 
 int carPos = WIN_WIDTH/2;
 int score =0;
@@ -41,12 +41,12 @@ void setcursor(bool visible, DWORD size){
 void drawBorder(){
     for(int i =0;i<SCREEN_HEIGHT; i++){
         for(int j =0;j<17;j++){
-            gotoxy(0+j,i); cout<<"+";
-            gotoxy(WIN_WIDTH-j,i); cout<<"+";
+            gotoxy(0+j,i); cout<<"±";
+            gotoxy(WIN_WIDTH-j,i); cout<<"±";
         }
     }
     for(int i =0;i<SCREEN_HEIGHT;i++){
-        gotoxy(SCREEN_WIDTH,i); cout<<"+";
+        gotoxy(SCREEN_WIDTH,i); cout<<"±";
     }
 }
 
@@ -188,6 +188,42 @@ void play(){
         if(enemyFlag[0] == 1){
             enemyY[0] += 1; 
         }
+        if(enemyFlag[1] == 1){
+            enemyY[1] += 1;
+        }
+        if(enemyY[0] > SCREEN_HEIGHT-4){
+            resetEnemy(0);
+            score++;
+            updateScore();
+        }
+        if(enemyY[1] > SCREEN_HEIGHT -4){
+            resetEnemy(1);
+            score++;
+            updateScore();
+        }
 
     } 
+}
+
+int main(){
+    setcursor(0,0);
+    srand((unsigned)time(NULL));
+
+    do{
+        system("cls");
+        gotoxy(10,5);cout<<"-----------------------------";
+        gotoxy(10,6);cout<<"|        Car Game           |";
+        gotoxy(10,7);cout<<"-----------------------------";
+        gotoxy(10,9);cout<<"1.Start Game";
+        gotoxy(10,10);cout<<"2.Instructions";
+        gotoxy(10,11);cout<<"3.Quit";
+        gotoxy(10,13);cout<<"Select Option: ";
+        char op = getche();
+
+        if(op =='1') play();
+        else if(op =='2') instructions();
+        else if(op =='3') exit(0);
+    }
+    while(1);
+    return 0;
 }
